8870cf6ff3379aa889ac53d520b1dc51bc3f3d8f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-18 19:47:54 +0200

    optimum solution to the year chaos algorithm.

--- Array-Algorithms/New_Year_Chaos/minimum_bribes.py
+++ Array-Algorithms/New_Year_Chaos/minimum_bribes.py
@@ -1,5 +1,22 @@
 def minimumBribes(q):
-    # Write your code here
-   pass
+    q = [i - 1 for i in q]  # set queue to start at 0
+
+    bribes = 0
+
+    for i, o in enumerate(q):
+
+        cur = i
+
+        if o - cur > 2:
+            print("Too chaotic")
+
+            return
+
+        for k in q[max(o - 1, 0):i]:
+
+            if k > o:
+                bribes += 1
+
+    print(bribes)
 
 print(minimumBribes([1 ,2 ,5 ,3 ,4 ,7 ,8 ,6]))

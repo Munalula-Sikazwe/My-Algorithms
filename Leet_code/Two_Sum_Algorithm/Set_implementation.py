8afd3dd47e3ecfd3867d41b6bca7fea4a02bfe30
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-18 17:58:58 +0200

    minor housekeeping

--- Leet_code/Two_Sum_Algorithm/Set_implementation.py
+++ Leet_code/Two_Sum_Algorithm/Set_implementation.py
@@ -1,13 +1,11 @@
 class Solution:
-    def twoSum(self, nums, target) :
+    def twoSum(self, nums, target):
         seen = set()
         for index, number in enumerate(nums):
             current_target = target - number
             for seen_index, seen_number in seen:
                 if seen_number == current_target:
                     return [index, seen_index]
-
-
 
 
             else:

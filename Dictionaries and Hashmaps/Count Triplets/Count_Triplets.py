650496f02cb6dc77a5cc5d1f6cbb5b9a93bc5ecb
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-19 16:12:01 +0200

    Created Count Triplets algorithm

---
+++ Dictionaries and Hashmaps/Count Triplets/Count_Triplets.py
@@ -0,0 +1,13 @@
+from collections import  Counter
+def countTriplets(arr, r):
+    a = Counter(arr)
+    b = Counter()
+    s = 0
+    for i in arr:
+        j = i//r
+        k = i*r
+        a[i]-=1
+        if b[j] and a[k] and i%r == 0:
+            s+=b[j]*a[k]
+        b[i]+=1
+    return s

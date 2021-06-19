2e985bcc44d68c3a7095287f8e58a0db5109881b
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-19 18:25:28 +0200

    Created my frequency queries solution

---
+++ Dictionaries and Hashmaps/Frequency Queries/Frequency Queries.py
@@ -0,0 +1,22 @@
+from collections import defaultdict, Counter
+
+
+# Complete the freqQuery function below.
+def freqQuery(queries):
+    output = []
+    container = []
+    frequency = defaultdict(int)
+    for operation, value in queries:
+        frequency = Counter(container)
+        if operation == 1:
+            container.append(value)
+        if operation == 2:
+            if value in container:
+                container.remove(value)
+        if operation == 3:
+            if value in frequency.values():
+                output.append(1)
+            else:
+                output.append(0)
+
+    return output

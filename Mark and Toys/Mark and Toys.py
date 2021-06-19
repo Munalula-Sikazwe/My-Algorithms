a89a84bc66e1733f9fbabf6225134472f921ced2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-19 22:02:39 +0200

    Well it's passed the first test cases.

---
+++ Mark and Toys/Mark and Toys.py
@@ -0,0 +1,9 @@
+def maximumToys(prices, k):
+    # Write your code here
+    bought = 0
+    prices = sorted(prices)
+    for price in prices:
+        if price < k:
+            k -= price
+            bought += 1
+    return bought 

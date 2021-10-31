6a7896ca4f009114b3e28d62ae9d3d59d6c8b72f
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-10-31 13:29:36 +0200

    converted index to _ to show it's value as an iterator count but not used in the code.

--- HackerRank/Sorting Algorithms/Bubble sort/Bubble sort.py
+++ HackerRank/Sorting Algorithms/Bubble sort/Bubble sort.py
@@ -2,7 +2,7 @@ def countSwaps(a):
     # Write your code here
     container = 0
     counter = 0
-    for index in range(len(a),0,-1):
+    for _ in range(len(a),0,-1):
         for subindex in range(len(a)-1):
             if a[subindex] > a[subindex+1]:
                 container = a[subindex]

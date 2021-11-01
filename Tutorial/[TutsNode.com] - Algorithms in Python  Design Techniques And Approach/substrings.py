12bb764f75cc8e2b628df0f19693ab4d16be232b
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-01 03:17:28 +0200

    completed the get substrings first run

---
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/substrings.py
@@ -0,0 +1,14 @@
+def get_substrings(string):
+    if len(string) == 0 :
+        return ""
+    else:
+        first_letter = string[0]
+        rest = get_substrings(string[1:])
+        result = ""
+
+        for substring in rest.split(","):
+            result += f",{first_letter}{substring}"
+            result += f",{substring}"
+
+        return result[1:]
+

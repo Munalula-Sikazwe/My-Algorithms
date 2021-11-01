0895ebf912a0ee791567a8f9c08f7ceecff2a417
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-01 06:04:24 +0200

    sub tree algorithm works fine.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/sub_tree.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/sub_tree.py
@@ -8,17 +8,17 @@ class Node:
 def inorder_traversal(tree, arr):
     if tree is None:
         return
-    inorder_traversal(tree.left, arr)
+    inorder_traversal(tree.left_node, arr)
     arr.append(tree.data)
-    inorder_traversal(tree.right, arr)
+    inorder_traversal(tree.right_node, arr)
 
 
 def preorder_traversal(tree, arr):
     if tree is None:
         return
     arr.append(tree.data)
-    inorder_traversal(tree.left, arr)
-    inorder_traversal(tree.right, arr)
+    inorder_traversal(tree.left_node, arr)
+    inorder_traversal(tree.right_node, arr)
 
 
 def is_subtree(tree, subtree):
@@ -31,13 +31,14 @@ def is_subtree(tree, subtree):
     inorder_traversal(tree, tree_array)
     inorder_traversal(subtree, subtree_array)
 
-    tree_array = "".join(tree_array)
-    subtree_array = "".join(subtree_array)
+    tree_array = "".join(map(str, tree_array))
+    subtree_array = "".join(map(str, subtree_array))
 
     if subtree_array in tree_array:
         return True
     else:
         return False
+
 
 main_tree = Node(1)
 main_tree.left_node = Node(2)
@@ -48,7 +49,7 @@ main_tree.right_node.left_node = Node(6)
 main_tree.right_node.right_node = Node(7)
 
 sub_tree = Node(3)
-sub_tree.left_node = Node(4)
-sub_tree.right_node = Node(5)
+sub_tree.left_node = Node(6)
+sub_tree.right_node = Node(7)
 
-print(is_subtree(main_tree,sub_tree))
+print(is_subtree(main_tree, sub_tree))

53dd9b76788db507f3fdd417e6a186fb933703bb
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-01 02:30:30 +0200

    completed tower of hanoi recursive solution first run.

---
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/tower_of_hanoi.py
@@ -0,0 +1,10 @@
+def tower_of_hanoi(number_of_rods, destination_rod, location_rod, auxiliary_rod):
+    if number_of_rods == 1:
+        print(f"moving rod number {number_of_rods} to {destination_rod} from {location_rod} ")
+    else:
+        tower_of_hanoi(number_of_rods - 1, auxiliary_rod, location_rod, destination_rod)
+        print(f"moving rod number {number_of_rods} to {auxiliary_rod} from {location_rod}.")
+        tower_of_hanoi(number_of_rods - 1, destination_rod, auxiliary_rod, location_rod)
+
+
+tower_of_hanoi(4, 3, 1, 2)

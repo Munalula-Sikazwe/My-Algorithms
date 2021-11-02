5d16977132db20447e1f7786a9f783a5bcfe762b
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-02 02:59:07 +0200

    created first run of the save queens backtracking algorithm.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/save_queens.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/save_queens.py
@@ -24,3 +24,28 @@ def is_safe_square(board, row, col, size):
         if board[rank][file] == 1:
             return False
     return True
+
+
+def save_queens(board, col, size):
+    if col >= size:
+        return True
+
+    for i in range(size):
+        if is_safe_square(board, i, col, size):
+            board[i][col] = 1
+
+            if save_queens(board, col + 1, size):
+                return True
+            ## perform backtracking
+
+            board[i][col] = 0
+
+
+board = [[0 for _ in range(5)] for _ in range(5)]
+size = len(board)
+
+if save_queens(board,0,size):
+    for i in board:
+        for j in i:
+            print(j, end=' ')
+        print()

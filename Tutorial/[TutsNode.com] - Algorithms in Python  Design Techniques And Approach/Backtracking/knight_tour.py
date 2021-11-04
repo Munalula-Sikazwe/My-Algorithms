4c36bf84b26300123fa8daeadb9670f91b61ca6a
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-04 02:36:50 +0200

    written the first test for the knights tour algorithm.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
@@ -7,7 +7,7 @@ def valid_move(board, row, column, n):
     return False
 
 
-def knight_tour(board, row, column, step, n=8):
+def knight_tour(board, step, row=0, column=0, n=8):
     path_x = [2, 1, -1, -2, -2, -1, 1, 2]
     path_y = [1, 2, 2, 1, -1, -2, -2, -1]
     if step == n ** 2:
@@ -25,4 +25,11 @@ def knight_tour(board, row, column, step, n=8):
 
 
 board = [[-1 for _ in range(n)] for _ in range(n)]
-
+board[0][0] = 0
+step = 1
+if knight_tour(board, step):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=' ')
+else:
+    print("Solution for this position does not exist.")

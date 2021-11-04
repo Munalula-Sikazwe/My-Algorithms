20d85c3421a92d1560ff4814c1300dc38ccc1642
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-04 02:50:35 +0200

    written the second test for the knights tour algorithm.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
@@ -1,4 +1,7 @@
+global path_x, path_y, n
 n = 8
+path_x = [2, 1, -1, -2, -2, -1, 1, 2]
+path_y = [1, 2, 2, 1, -1, -2, -2, -1]
 
 
 def valid_move(board, row, column, n):
@@ -8,8 +11,6 @@ def valid_move(board, row, column, n):
 
 
 def knight_tour(board, step, row=0, column=0, n=8):
-    path_x = [2, 1, -1, -2, -2, -1, 1, 2]
-    path_y = [1, 2, 2, 1, -1, -2, -2, -1]
     if step == n ** 2:
         return True
 
@@ -17,11 +18,11 @@ def knight_tour(board, step, row=0, column=0, n=8):
         row_new = row + path_x[index]
         column_new = column + path_y[index]
         if valid_move(board, row_new, column_new, n):
-            board[row_new, column_new] = step
+            board[row_new][column_new] = step
             if knight_tour(board, row, column, step + 1, n):
                 return True
-            board[row_new, column_new] = -1
-        return False
+            board[row_new][column_new] = -1
+    return False
 
 
 board = [[-1 for _ in range(n)] for _ in range(n)]
@@ -31,5 +32,6 @@ if knight_tour(board, step):
     for i in range(n):
         for j in range(n):
             print(board[i][j], end=' ')
+    print()
 else:
     print("Solution for this position does not exist.")

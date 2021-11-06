f31d1b49f52701182fa73157bcec070b56907e06
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-06 04:33:38 +0200

    knights tour algorithm finally works. for a 5x5 anyway.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
@@ -1,29 +1,26 @@
-
-n = 8
-
+board_size = 5
 
 
-def valid_move(board, row, column, n):
+def valid_move(board, row, column, n=5):
     if 0 <= row < n and 0 <= column < n and board[row][column] == -1:
         return True
-
     return False
 
 
-def knight_tour(board, step, row=0, column=0, n=8):
+def knight_tour(board, step, board_size, row=0, column=0, n=8):
     path_x = [2, 1, -1, -2, -2, -1, 1, 2]
     path_y = [1, 2, 2, 1, -1, -2, -2, -1]
-    if step == (n ** 2):
+    if step == board_size ** 2:
         print("reached edge case.")
         return True
 
     for index in range(n):
         row_new = row + path_x[index]
         column_new = column + path_y[index]
-        if valid_move(board, row_new, column_new, n):
+        if valid_move(board, row_new, column_new, n=board_size):
+            print("Valid Move")
             board[row_new][column_new] = step
-            if knight_tour(board, step + 1, row=row_new, column=column_new, n=n):
-                print(f"success:{row},{column}")
+            if knight_tour(board, step + 1, board_size=board_size, row=row_new, column=column_new):
                 return True
 
             board[row_new][column_new] = -1
@@ -31,17 +28,17 @@ def knight_tour(board, step, row=0, column=0, n=8):
     return False
 
 
-board = [[-1 for _ in range(n)] for _ in range(n)]
+board = [[-1 for _ in range(board_size)] for _ in range(board_size)]
 board[1][1] = 0
 step = 1
-if knight_tour(board, step, row=1, column=1):
-    for i in range(n):
-        for j in range(n):
+if knight_tour(board, step, board_size, row=1, column=1):
+    for i in range(board_size):
+        for j in range(board_size):
             print(board[i][j], end=' ')
         print()
 else:
-    for i in range(n):
-        for j in range(n):
+    for i in range(board_size):
+        for j in range(board_size):
             print(board[i][j], end=' ')
         print()
     print("Solution for this position does not exist.")

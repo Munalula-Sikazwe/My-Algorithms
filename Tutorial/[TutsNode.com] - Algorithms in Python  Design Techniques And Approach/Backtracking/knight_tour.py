a8002dc37d95452a8c64511f390ad20aba06ec22
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-06 04:39:27 +0200

    refactored bounding function.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Backtracking/knight_tour.py
@@ -1,7 +1,7 @@
 board_size = 5
 
 
-def valid_move(board, row, column, n=5):
+def valid_move(board, row, column, n):
     if 0 <= row < n and 0 <= column < n and board[row][column] == -1:
         return True
     return False
@@ -17,7 +17,7 @@ def knight_tour(board, step, board_size, row=0, column=0, n=8):
     for index in range(n):
         row_new = row + path_x[index]
         column_new = column + path_y[index]
-        if valid_move(board, row_new, column_new, n=board_size):
+        if valid_move(board, row_new, column_new, board_size):
             board[row_new][column_new] = step
             if knight_tour(board, step + 1, board_size, row=row_new, column=column_new):
                 return True

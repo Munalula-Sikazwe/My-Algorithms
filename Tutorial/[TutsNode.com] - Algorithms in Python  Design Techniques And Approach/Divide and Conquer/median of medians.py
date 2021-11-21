1c068ec2a8bada4155efab5ec6a9fe410600a3a0
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-22 00:51:28 +0200

    created the find median , median utility and partition function for the median of medians algorithm.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Divide and Conquer/median of medians.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Divide and Conquer/median of medians.py
@@ -1,9 +1,13 @@
+from math import ceil
+
+
 def find_median(arr):
     n = len(arr)
     mid_point = (n / 2) + 1
 
     median = median_utility(arr, mid_point, 0, n)
     print(median)
+
 
 def median_utility(arr, mid_point, low_point, high_point):
     m = partition(arr, low_point, high_point)
@@ -21,3 +25,12 @@ def partition(arr, low_point, high_point):
         arr[low_point], arr[high_point] = arr[high_point], arr[low_point]
 
     return high_point
+
+
+def get_pivot(arr, low_point, high_point):
+    medians = [0] * int(ceil(high_point - low_point + 1) / 5)
+
+    median_index = 0
+
+    while low_point <= high_point:
+        temp = [0] * min(5, high_point - low_point + 1)

99d8c00dd7340097f83aecfa0370a14a4a1630c4
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-24 23:44:07 +0200

    created the intervals sheduling function.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Greedy/interval_sheduling.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Greedy/interval_sheduling.py
@@ -8,3 +8,22 @@ intervals = [
     [16, 20],
 
 ]
+
+
+def interval_sheduling(intervals):
+    intervals.sort(key=lambda i: i[1])
+
+    last_finish_time = float("-inf")
+    optimal_intervals = []
+    for interval in intervals:
+        start = interval[0]
+        if start > last_finish_time:
+            end = interval[1]
+            optimal_intervals.append(interval)
+            last_finish_time = end
+
+    return optimal_intervals
+
+
+
+

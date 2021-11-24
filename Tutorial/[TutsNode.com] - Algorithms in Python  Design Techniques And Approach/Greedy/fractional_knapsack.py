c67c96bc4c1169ddb82772a984fee7674d5e6854
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-24 16:47:35 +0200

    created the items class

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Greedy/fractional_knapsack.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Greedy/fractional_knapsack.py
@@ -1,4 +1,4 @@
-wt = [10, 40, 20, 20]
+wt = [10, 40, 20, 30]
 val = [60, 40, 100, 120]
 capacity = 50
 
@@ -6,7 +6,21 @@ capacity = 50
 def get_max_value(wt, val, capacity):
     items = [0] * len(wt)
     for i in range(0, len(wt)):
-        item[i] = Item()
+        items[i] = Item(wt[i], val[i], i)
+    total_value = 0
+    items.sort(reverse=True)
+    for item in items:
+        print(total_value, item.wt, item.val,capacity)
+        cur_wt = item.wt
+        cur_val = item.val
+
+        if capacity - cur_wt >= 0:
+            capacity -= cur_wt
+            total_value += cur_val
+        else:
+            total_value += cur_val * (capacity / cur_wt)
+            break
+    return total_value
 
 
 class Item:
@@ -18,3 +32,6 @@ class Item:
 
     def __lt__(self, other):
         return self.cost < other.cost
+
+
+print(get_max_value(wt, val, capacity))

9fc4320b0d1acfa16cbaf0ed41e0f717ee403427
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-11-26 20:14:57 +0200

    created the huffman node.

--- Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Greedy/huffman_code.py
+++ Tutorial/[TutsNode.com] - Algorithms in Python  Design Techniques And Approach/Greedy/huffman_code.py
@@ -1,4 +1,5 @@
 str = 'ABBCCCCGGGGDEAAAEDBBBDFAGG'
+char_binary_mapping = {}
 
 
 class HuffmanNode:
@@ -9,6 +10,38 @@ class HuffmanNode:
         self.right = right
 
 
+def generate_tree(mapping):
+    keyset = mapping.keys()
+    priority_queue = []
+    for key in keyset:
+        node = HuffmanNode(mapping[key], key, None, None)
+        priority_queue.append(node)
+        priority_queue = sorted(priority_queue, key=lambda x: x.freq)
+        while len(priority_queue) > 1:
+            first = priority_queue.pop(0)
+            second = priority_queue.pop(0)
+            merge_node = HuffmanNode(first.freq + second.freq, '-', first, second)
+            priority_queue.append(merge_node)
+            priority_queue = sorted(priority_queue, key=lambda x: x.freq)
+    return priority_queue.pop()
+
+
+def set_binary_code(node, str):
+    if node:
+        if node.left is None and node.right is None:
+            char_binary_mapping[node.data] = str
+    # left
+    if node.left:
+        str += '0'
+        set_binary_code(node.left, str)
+        str = str[:-1]
+    # right
+    if node.right:
+        str += '1'
+        set_binary_code(node.right, str)
+        str = str[:-1]
+
+
 def huffman_code(str):
     mapping = {}
     for character in str:
@@ -17,4 +50,16 @@ def huffman_code(str):
         else:
             mapping[character] += 1
 
-    generate_tree(mapping)
+    root = generate_tree(mapping)
+    set_binary_code(root, '')
+    for character in mapping:
+        print('%-4r |%12s' % (character, char_binary_mapping[character]))
+
+    s = ''
+
+    for character in str:
+        s += char_binary_mapping[character]
+    return s
+
+
+print(huffman_code(str))
